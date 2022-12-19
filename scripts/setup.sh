d97df0b6c5e726c695b38c18d63d71d2234266d6
bishop <bishop@Pratiks-MacBook-Air.local>
2022-12-19 15:10:25 +0530

    updating script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,9 +5,8 @@ rm -rf ~/.boli
 mkdir ~/.boli
 
 boli init --chain-id test test
-boli keys add test --recover --keyring-backend test<<<"y
-wage thunder live sense resemble foil apple course spin horse glass mansion midnight laundry acoustic rhythm loan scale talent push green direct brick please"
-boli add-genesis-account test 100000000000000stake --keyring-backend test
-boli gentx test 1000000000stake --chain-id test --keyring-backend test
+boli keys add cooluser --keyring-backend test
+boli add-genesis-account cooluser 100000000000000stake --keyring-backend test
+boli gentx cooluser 1000000000stake --chain-id test --keyring-backend test
 boli collect-gentxs
 boli start
